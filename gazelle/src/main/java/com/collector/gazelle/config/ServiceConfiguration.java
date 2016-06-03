20884c9db4de8787a3a14e998b721cec5ce42f0c
suresh <sureshchintamjntu504@gmail.com>
2016-06-04 01:31:50 +0530

    Added configPath property

--- gazelle/src/main/java/com/collector/gazelle/config/ServiceConfiguration.java
+++ gazelle/src/main/java/com/collector/gazelle/config/ServiceConfiguration.java
@@ -8,9 +8,15 @@ import com.yammer.dropwizard.config.Configuration;
 public class ServiceConfiguration extends Configuration {
 
     private AppConfig appconfig;
+    String configPath;
     
     @JsonProperty("appconfig")
     public AppConfig getAppconfig(){
         return appconfig;
     }
+    
+    @JsonProperty("configPath")
+    public String getConfigPath() {
+		return configPath;
+	}
 }
